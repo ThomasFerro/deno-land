21d4523567f640f2398d8597a789d8c54cdd1c01
Thomas Ferro <t.ferro184@gmail.com>
2020-06-29 14:39:46 +0200

    Core (cli): Finishing simple cli

--- index.ts
+++ index.ts
@@ -11,45 +11,54 @@ TODO:
 */
 
 interface Command {
-    message: string,
-    handler: (park: Park, payload: string) => Park,
+  message: string;
+  handler: (park: Park, payload: string) => Park;
 }
 interface Commands {
-    [commandName: string]: Command
+  [commandName: string]: Command;
 }
 const commands: Commands = {
-    'B': {
-        message: 'Type B <first dino> <second dino> <name> to breed (eg: "B 0 1 Sam" to breed the first two dinosaurs and name the new one "Sam")',
-        handler: (park: Park, payload: string): Park => {
-            const [ _, firstDeno, secondDeno, name ] = payload.split(' ');
-            return park.breed(
-                park.dinosaurs[Number(firstDeno)],
-                park.dinosaurs[Number(secondDeno)],
-                name
-            );
-        },
+  "B": {
+    message:
+      'Type B <first dino> <second dino> <name> to breed (eg: "B 0 1 Sam" to breed the first two dinosaurs and name the new one "Sam")',
+    handler: (park: Park, payload: string): Park => {
+      const [_, firstDeno, secondDeno, name] = payload.split(" ");
+      return park.breed(
+        park.dinosaurs[Number(firstDeno)],
+        park.dinosaurs[Number(secondDeno)],
+        name,
+      );
     },
-    'E': {
-        message: 'Press E to euthanize a dinosaur',
-        handler: (park: Park, payload: string): Park => {
-            console.log('TODO');
-            return park;
-        },
+  },
+  "E": {
+    message:
+      'Type E <dino> to euthanize a dinosaur (eg: "E 1" to euthanize the dinosaur at index 1)',
+    handler: (park: Park, payload: string): Park => {
+      const [_, index] = payload.split(" ");
+      return park.euthanize(park.dinosaurs[Number(index)]);
     },
-    'F': {
-        message: 'Press F to feed a dinosaur',
-        handler: (park: Park, payload: string): Park => {
-            console.log('TODO');
-            return park;
-        },
-    }
-}
+  },
+  "F": {
+    message:
+      'Type F <dino> to feed a dinosaur (eg: "F 2" to feed the dinosaur at index 2)',
+    handler: (park: Park, payload: string): Park => {
+      const [_, index] = payload.split(" ");
+      return park.feed(park.dinosaurs[Number(index)]);
+    },
+  },
+};
 
 const updateDisplay = (park: Park) => {
   console.clear();
   console.log("Welcome... to Deno Park !");
   park.dinosaurs.forEach((dinosaur: Dinosaur, index: number) => {
-    console.log(`[${index}] ${dinosaur.name} - ${dinosaur.hunger}`);
+    console.log(
+      `[${index}] ${
+        dinosaur.isAlive
+          ? "🙂"
+          : "💀"
+      } ${dinosaur.name} - ${dinosaur.hunger}`,
+    );
   });
   Object
     .values(commands)
@@ -65,16 +74,16 @@ setInterval(() => {
 }, 6000);
 
 for await (const command: string of readLines(Deno.stdin)) {
-    let error = null
-    if (command && command.length > 0 && commands[command[0]]) {
-        try {
-            park = commands[command[0]].handler(park, command);
-        } catch (e) {
-            error = e.message
-        }
+  let error = null;
+  if (command && command.length > 0 && commands[command[0]]) {
+    try {
+      park = commands[command[0]].handler(park, command);
+    } catch (e) {
+      error = e.message;
     }
-    updateDisplay(park);
-    if (error) {
-        console.log('Error:', error);
-    }
+  }
+  updateDisplay(park);
+  if (error) {
+    console.log("Error:", error);
+  }
 }
