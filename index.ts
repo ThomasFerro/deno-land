93f6c1a571fe924ef638c417ec87781582c3baa8
Thomas Ferro <t.ferro184@gmail.com>
2020-06-30 14:48:18 +0200

    Core (mode): switch between http and cli modes

--- index.ts
+++ index.ts
@@ -2,4 +2,9 @@ import { initiatePark } from "./domain/park.ts";
 import { initiateCli } from "./cli/index.ts";
 
 const park = initiatePark();
-initiateCli(park);
+if (Deno.args.indexOf('--http') !== -1) {
+  console.log('UNIMPLEMENTED');
+} else {
+  initiateCli(park);
+}
+
